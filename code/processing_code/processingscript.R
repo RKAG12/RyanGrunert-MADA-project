5b0fc65f10265c831f987b1322444f5053413d41
Ryan <grunertryan@gmail.com>
2021-09-22 15:42:28 -0400

    Processing Script Update 9/22/21
    Finished the majority of the processing and data tidying for Ibis blood parasite 2010-14 data. Still need to finish cleaning some of the date data and then exploratory graphing.

--- code/processing_code/processingscript.R
+++ code/processing_code/processingscript.R
@@ -1,66 +1,74 @@
 ###############################
-# processing script
-#
-#this script loads the raw data, processes and cleans it 
-#and saves it as Rds file in the processed_data folder
+# Processing Script
 
-#load needed packages. make sure they are installed.
-library(readxl) #for loading Excel files
-library(dplyr) #for data processing
+
+########Loading all required packages
 library(here) #to set paths
+library(tidyverse) #all required data manipulation packages
+library(xlsx) #to read in excel spreadsheets
+library(lubridate) #to extract times from raw data
+library(janitor) #to help clean up dates and tidy data
 
-#path to data
-#note the use of the here() package and not absolute paths
-data_location <- here::here("data","raw_data","exampledata.xlsx")
+########Inputting all the raw data into R
+#Setting the path to the Ibis blood parasite spreadsheet
+data_locationIbisB <- here::here("data","raw_data","Ibis_Blood_ParasiteData_2017.xlsx")
+#Loads the raw Ibis blood data excel sheet from 2015-2017
+rdIbisBlood15_17 <- read.xlsx(data_locationIbisB, sheetName = "2015-2017")
+#Loads the raw Ibis blood data excel sheet from 2010-2014
+rdIbisBlood10_14 <- read.xlsx(data_locationIbisB, sheetName = "2010-2014")
+#Setting the path to the Ibis field data spreadsheet
+data_locationIbisF <- here::here("data","raw_data","IbisFieldDataOct19_2017.xlsx")
+#Loads the raw Ibis field data excel sheet
+rdIbisField <- read.xlsx(data_locationIbisF, sheetName = "All capture data")
+#Setting the path to the first Ibis supplemental excel sheet (Urban Gradients)
+data_locationIbisUr <- here::here("data","raw_data","100IbisID_UrbanGradient.xlsx")
+#Loads the raw Ibis Urban Gradient supplemental data sheet
+UrbanIbis <- read.xlsx(data_locationIbisUr, sheetName = "Samples by Date")
+#Setting the path to the second Ibis supplemental excel sheet (Salmonella)
+data_locationIbisSal <- here::here("data","raw_data","SalmonellaFieldSerotypesAug27_2018.xlsx")
+#Loads the raw Ibis Urban Gradient supplemental data sheet
+SalIbis <- read.xlsx(data_locationIbisSal, sheetName = "ALL Positives")
 
-#load data. 
-#note that for functions that come from specific packages (instead of base R)
-# I often specify both package and function like so
-#package::function() that's not required one could just call the function
-#specifying the package makes it clearer where the function "lives",
-#but it adds typing. You can do it either way.
-rawdata <- readxl::read_excel(data_location)
+########Tidying the rdIbisBlood10_14 data
+IbisBlood10_14 <- rename(rdIbisBlood10_14, ID = X.) #renames the ID column
 
-#take a look at the data
-dplyr::glimpse(rawdata)
+#Extracts just the columns of interest
+IbisBlood10_14 <- select(IbisBlood10_14, ID, Site.ID, Date, PCR.sex, Age..adult.juvenile., 
+                         Total.RBC, Haemoproteus.parasitemia.)
 
-#dataset is so small, we can print it to the screen.
-#that is often not possible.
-print(rawdata)
+#Renames the columns of interest
+IbisBlood10_14 <- rename(IbisBlood10_14, Site = Site.ID, Sex = PCR.sex, Age = Age..adult.juvenile.,
+                         TotalRBC = Total.RBC, HaeParasit = Haemoproteus.parasitemia.)
 
-# looks like we have measurements for height (in centimeters) and weight (in kilogram)
+#Change "adult" and "juvenile" in the Age column to "A" and "J"
+IbisBlood10_14$Age[IbisBlood10_14$Age == "adult"] <- "A"
+IbisBlood10_14$Age[IbisBlood10_14$Age == "Adult"] <- "A"
+IbisBlood10_14$Age[IbisBlood10_14$Age == "juvenile"] <- "J"
+IbisBlood10_14$Age[IbisBlood10_14$Age == "SA"] <- "A"
+#Now the Ibises that had age recorded are referred to as "A" for adult or "J" for juvenile
 
-# there are some problems with the data: 
-# There is an entry which says "sixty" instead of a number. 
-# Does that mean it should be a numeric 60? It somehow doesn't make
-# sense since the weight is 60kg, which can't happen for a 60cm person (a baby)
-# Since we don't know how to fix this, we need to remove the person.
-# This "sixty" entry also turned all Height entries into characters instead of numeric.
-# We need to fix that too.
-# Then there is one person with a height of 6. 
-# that could be a typo, or someone mistakenly entered their height in feet.
-# Since we unfortunately don't know, we'll have to remove this person.
-# similarly, there is a person with weight of 7000, which is impossible,
-# and one person with missing weight.
-# to be able to analyze the data, we'll remove those 5 individuals
 
-# this is one way of doing it. Note that if the data gets updated, 
-# we need to decide if the thresholds are ok (newborns could be <50)
+#Changing the DIV/0 values in HaeParasit to NA and change the column to numerical values
+IbisBlood10_14$HaeParasit <- as.numeric(IbisBlood10_14$HaeParasit)  # Divide-by-zero errors are now NA
 
-processeddata <- rawdata %>% dplyr::filter( Height != "sixty" ) %>% 
-                             dplyr::mutate(Height = as.numeric(Height)) %>% 
-                             dplyr::filter(Height > 50 & Weight < 1000)
+#Reformatting the date values in the Date column
+IbisBlood10_14$Date <- excel_numeric_to_date(IbisBlood10_14$Date)
+#After this step, the values that just state the year are labelled with "1905", may need to go back or exclude from analysis
 
-# save data as RDS
-# I suggest you save your processed and cleaned data as RDS or RDA/Rdata files. 
-# This preserves coding like factors, characters, numeric, etc. 
-# If you save as CSV, that information would get lost.
-# See here for some suggestions on how to store your processed data:
-# http://www.sthda.com/english/wiki/saving-data-into-r-data-format-rds-and-rdata
-
-# location to save file
-save_data_location <- here::here("data","processed_data","processeddata.rds")
-
-saveRDS(processeddata, file = save_data_location)
+#Abbreviating the site names to make analysis easier
+IbisBlood10_14$Site[IbisBlood10_14$Site == "Royal Palms"] <- "RP"
+IbisBlood10_14$Site[IbisBlood10_14$Site == "Everglades"] <- "E"
+IbisBlood10_14$Site[IbisBlood10_14$Site == "Dreher Park"] <- "DP"
+IbisBlood10_14$Site[IbisBlood10_14$Site == "Indian Creek"] <- "ICP"
+IbisBlood10_14$Site[IbisBlood10_14$Site == "Indian Creek "] <- "ICP"
+IbisBlood10_14$Site[IbisBlood10_14$Site == "Indian Creek Park"] <- "ICP"
+IbisBlood10_14$Site[IbisBlood10_14$Site == "Juno Beach"] <- "JB"
+IbisBlood10_14$Site[IbisBlood10_14$Site == "Lion Country Safari"] <- "LCS"
+IbisBlood10_14$Site[IbisBlood10_14$Site == "Lake Worth"] <- "LW"
+IbisBlood10_14$Site[IbisBlood10_14$Site == "Prosperity Oaks"] <- "PO"
+IbisBlood10_14$Site[IbisBlood10_14$Site == "Promenade Plaza"] <- "PP"
+IbisBlood10_14$Site[IbisBlood10_14$Site == "San Mateo"] <- "SM"
+IbisBlood10_14$Site[IbisBlood10_14$Site == "Solid Waste Authority"] <- "SWA"
+IbisBlood10_14$Site[IbisBlood10_14$Site == "West Palm Beach Zoo"] <- "WPBZ"
 
 
